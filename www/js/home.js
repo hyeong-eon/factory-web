f010bead34736125b13c2ff4e1cd5ac7ddb7bafa
형언 <ilsin302a@gmail.com>
2025-12-11 10:48:35 +0900

    홈 변경

--- www/js/home.js
+++ www/js/home.js
@@ -3,7 +3,11 @@ $(document).ready(function () {
   $("#navs").load("../components/nav.html");
 });
 function testDeeplink(page) {
-  const url = `factoryapp://attendance/${page}`;
-  console.log("딥 링크 호출:", url);
-  window.location.href = url;
+  // 앱에서는 직접 페이지 이동
+  if (window.Capacitor) {
+    window.location.href = `./${page}.html`;
+  } else {
+    // 웹에서는 HTTPS 링크
+    window.location.href = `https://factory-web-mu.vercel.app/main/${page}.html`;
+  }
 }
